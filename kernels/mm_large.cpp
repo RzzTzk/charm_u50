0b719a791d073e90954a5bd7520859b07383da36
Xing,Wanqi <wanqi.xing@mailbox.tu-dresden.de>
2025-08-13 14:30:05 +0200

    1

--- kernels/mm_large.cpp
+++ kernels/mm_large.cpp
@@ -6,6 +6,29 @@
 #define TILE_K 128
 #define DSP_PER_PE 4
 
+template<typename T, int DIM1, int DIM2>
+void read_block(const T* src, T dst[DIM1][DIM2], int rows, int cols, int ld) {
+    #pragma HLS INLINE
+    for (int i = 0; i < rows; i++) {
+        #pragma HLS PIPELINE II=1
+        for (int j = 0; j < cols; j++) {
+            dst[i][j] = src[i*ld + j];
+        }
+    }
+}
+
+template<typename T, int DIM1, int DIM2>
+void write_block(T* dst, const T src[DIM1][DIM2], int rows, int cols, int ld) {
+    #pragma HLS INLINE
+    for (int i = 0; i < rows; i++) {
+        #pragma HLS PIPELINE II=1
+        for (int j = 0; j < cols; j++) {
+            dst[i*ld + j] = src[i][j];
+        }
+    }
+}
+
+
 extern "C" {
 void mm_large(
     const float* A,  // HBM通道0-7
